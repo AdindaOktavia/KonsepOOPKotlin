905ea0aea79088970bdccd4a290199d6496ab508
Adinda Oktavia <adindaoktaviabks0812@gmail.com>
2024-09-28 12:30:33 +0700

    Add NestedInnerClassB.kt

---
+++ src/main/kotlin/id/ac/polbeng/adindaoktavia/test_kelas/NestedInnerClassB.kt
@@ -0,0 +1,13 @@
+package id.ac.polbeng.adindaoktavia.test_kelas
+
+fun main() {
+    var programmer: Human = object : Human {
+        override fun think() {
+            print("I am an example of Anonymous Inner Class ")
+        }
+    }
+    programmer.think()
+}
+interface Human {
+    fun think()
+}
